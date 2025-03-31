0ac6bbc6b03cd15ffb6f7fbecbda26c397c1fc84
Andrew <56172745+JediWebDev@users.noreply.github.com>
2025-03-31 13:36:53 -0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,15 @@ import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center py-4 px-4 border-b">
-      <Link href="/" className="text-xl font-bold">MyGallery</Link>
+      <Link href="/" className="text-xl font-bold">Squirrely Tails</Link>
       <div className="flex gap-4 items-center">
         <Link href="/gallery">Gallery</Link>
-        <Link href="/subscribe">Subscribe</Link>
-        <Link href="/account">
+        <Link href="/gallery">Comics</Link>
+        <Link href="/gallery">Games</Link>
+      </div>
+      <div className="flex gap-4 items-center">
+      <Link href="/subscribe">Subscribe</Link>
+      <Link href="/account">
           <Avatar>
             <AvatarImage src="@/public/avatar-icon.png"/>
             <AvatarFallback>U</AvatarFallback>
